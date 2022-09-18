129c5eb28924ccced7902f7b94aa6a54c7179d75
Viresh2217 <83824549+Viresh2217@users.noreply.github.com>
2022-09-18 10:38:00 +0530

    Project_Spring_MVC

---
+++ Project_Spring_MVC/src/main/java/Com/Spring/Controller/HomeController.java
@@ -0,0 +1,24 @@
+package Com.Spring.Controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController{
+
+	@RequestMapping("/home")
+	public String home() {
+
+		System.out.println("Hello..");
+		return "index";
+	}
+	
+	@RequestMapping("/about")
+	public String about() 
+	{
+		System.out.println("This is about......");
+		return "about";
+	}
+
+};
---
+++ Project_Spring_MVC/src/main/java/Com/Spring/MVC/New.java
@@ -0,0 +1,5 @@
+package Com.Spring.MVC;
+
+public class New {
+
+}
